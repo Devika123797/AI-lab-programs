db35bcebff27371c7290354f9b560951dfdcc40e
Devika123797 <devika.askvr@gmail.com>
2025-04-07 20:25:37 +0530

    Update CSP.py

--- CSP.py
+++ CSP.py
@@ -1,27 +1,87 @@
-def objective_function(x):
-    return -x**2+4*x
-def get_neighbors(x, step_size):
-    return [x + step_size, x - step_size]
-def hill_climbing (start, step_size=0.1, max_iteration = 5):
-    current_state=start
-    current_value = objective_function(current_state)
-    print(f"Initial state (x) :{current_state}")
-    for iteration in range (max_iteration):
-        neighbors = get_neighbors(current_state , step_size )
-        best_neighbor=None
-        best_value=current_value
-        for neighbor in neighbors:
-            neighbor_value=objective_function(neighbor)
-            if neighbor_value >best_value:
-                best_neighbor = neighbor
-                best_value = neighbor_value
-        if best_neighbor is not None and best_value > current_value:
-            current_state = best_neighbor
-            current_value=best_value
-            print(f"Iteration {iteration +1}: New current state (x):{current_state},f(x):{current_value}")
-        else:
-            print(f"Iteration {iteration + 1}: No better neighbors ,stopping.")
-            break
-    print(f"Final state(x):{current_state}")
-    print(f" Final value of f(x):{current_value}")
-hill_climbing(start = 0, step_size = 0.1, max_iteration=100)
+def print_sol(): 
+
+   for i in range(n): 
+
+       for j in range(n): 
+
+           print(a[i][j], end=" ") 
+
+       print() 
+
+   print() 
+
+ 
+
+def is_safe(row, col): 
+
+   # checking for the same row 
+
+   for i in range(col): 
+
+       if a[row][i]: 
+
+           return False 
+
+ 
+
+   # checking for the same column 
+
+   for i in range(row): 
+
+       if a[i][col]: 
+
+           return False 
+
+ 
+
+   # checking for the left diagonal 
+
+   for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)): 
+
+       if a[i][j]: 
+
+           return False 
+
+ 
+
+   # checking for the right diagonal 
+
+   for i, j in zip(range(row - 1, -1, -1), range(col + 1, n)): 
+
+       if a[i][j]: 
+
+           return False 
+
+ 
+
+   return True 
+
+ 
+
+def nqueen(row): 
+
+   if row == n: 
+
+       print_sol() 
+
+   else: 
+
+       for i in range(n): 
+
+           if is_safe(row, i): 
+
+               a[row][i] = 1 
+
+               nqueen(row + 1) 
+
+               a[row][i] = 0 
+
+ 
+
+#main program 
+
+n = int(input("Enter the value of N :: ")) 
+
+a = [[0] * 10 for _ in range(10)] 
+
+nqueen(0) 
